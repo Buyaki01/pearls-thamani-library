4053da3ca57700ec87a4823ce6193f052bf39e8a
ritta.sweta <ritta.sweta@savannahinformatics.com>
2025-04-19 12:24:40 +0300

    Add Auth UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,7 +8,9 @@ const layout = ({ children }: { children: ReactNode }) => {
         <div className="auth-box">
           <div className="flex flex-row gap-3">
             <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
-            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+            <h1 className="text-2xl font-semibold text-white">
+              Pearls Thamani Library
+            </h1>
           </div>
         </div>
       </section>
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>Sign Up Form</div>;
+import React from "react";
+import AuthForm from "@/components/AuthForm";
+import { signUpSchema } from "@/lib/validations";
+
+const Page = () => {
+  <AuthForm
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+      fullName: "",
+      universityId: 0,
+      universityCard: "",
+    }}
+    onSubmit={async () => ({ success: true })}
+  />;
 };
 
-export default page;
+export default Page;
