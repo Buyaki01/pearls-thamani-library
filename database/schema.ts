c0c1c033e8f0d9a20525ff5fe1d49da324b97630
ritta.sweta <ritta.sweta@savannahinformatics.com>
2025-04-23 17:24:45 +0300

    Add drizzle orm, drizzle-kit and @neondatabase/serverless

---
+++ database/schema.ts
@@ -0,0 +1,16 @@
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  fullName: varchar("full_name", { length: 255 }).notNull(),
+  email: text("email").notNull().unique(),
+  universityId: integer("university_id").notNull().unique(),
+});
